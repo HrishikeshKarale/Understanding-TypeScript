c4c90214eb1a4c19d9fab8d835da1b3d30c63699
HrishikeshKarale <Hrishikesh.karale@gmail.com>
2021-01-12 11:08:48 -0500

    added lite-server and a add function to app.ts

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log("This is a ts file");
+function add(num1, num2) {
+    return num1 + num2;
+}
+;
+var num1 = 20, num2 = 8;
+var result = add(num1, num2);
+console.log(result);
---
+++ app.ts
@@ -0,0 +1,11 @@
+// console.log("This is a ts file"); //running lite-srver
+
+function add(num1, num2) {
+	return num1 + num2;
+};
+
+const num1 = 20,
+	num2 = 8;
+const result = add(num1, num2);
+
+console.log(result);
